b3a06ac4f98f0c2cf8950aba8daf855cdc93a844
Sidney François <sidney.fvm.ps@outlook.com>
2022-05-30 14:39:29 -0300

    inserindo horário nas tags html, inserindo lógica para o horário.

--- scripts.js
+++ scripts.js
@@ -9,4 +9,24 @@ setInterval(() => {
 	hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
 	mn.style.transform = `rotateZ(${mm}deg)`;
 	sc.style.transform = `rotateZ(${ss}deg)`;
+
+  let hhDigital = document.getElementById('hour');
+	let mmDigital = document.getElementById('minutes');
+	let ssDigital = document.getElementById('seconds');
+	let ampm = document.getElementById('ampm');
+	let h = day.getHours();
+	let m = day.getMinutes();
+	let s = day.getSeconds();
+
+	var dayTime = h >= 12 ? 'PM' : 'AM'; 
+		  
+	h = (h < 10) ? "0" + h : h
+	m = (m < 10) ? "0" + m : m
+	s = (s < 10) ? "0" + s : s
+
+
+	hhDigital.innerHTML = h;
+	mmDigital.innerHTML = m;
+	ssDigital.innerHTML = s;
+	ampm.innerHTML = dayTime;
 })
